239bd3334da2c3391e424a65db3cb0c60ade1c7a
Sudip Das <sudipkgec23@gmail.com>
2026-05-10 02:53:53 +0530

    2 advance problems solve

--- check_PerfectNumber.js
+++ check_PerfectNumber.js
@@ -9,7 +9,7 @@
 
     let sum = 0;
     
-    for ( let i = 1 ; i <= num / 2 ; i++){
+    for ( let i = 1 ; i <= Math.floor(num / 2) ; i++){
         if ( num % i === 0){
             sum += i;
      }
